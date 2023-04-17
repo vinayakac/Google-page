279da03bce59ed18a2c97102ea5f568aaf64021a
ROOPAGOUDA12 <roopaggouda@gmail.com>
2023-04-17 11:30:30 +0530

    React pinterest loginpage

---
+++ pinterest5/pinterest/src/App.js
@@ -0,0 +1,12 @@
+// import logo from './logo.svg';
+// import './App.css';
+import Login from './components/Login.jsx'
+
+
+function App() {
+  return (
+    <Login></Login>
+  );
+}
+
+export default App;
---
+++ pinterest5/pinterest/src/components/Login.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import "../components/Login.css"
+import Header from './Header.jsx'
+
+export default function login(){
+    return(
+    <div className="page-container">
+        <div className="login-container">
+            <div>
+                <Header />
+            </div>
+        </div>
+
+    </div>
+    );
+}
+
+
+
+// export default Login;
