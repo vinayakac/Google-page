4ddc2ed00fccf6099df596946cea0d9cec68e604
ROOPAGOUDA12 <roopaggouda@gmail.com>
2023-04-27 09:57:46 +0530

    BALADI project Why Us and About Us page in react

--- project/src/Components/About1.jsx
+++ project/src/Components/About1.jsx
@@ -7,7 +7,12 @@ export default function Next() {
             <div className='part2'>
                 
                 <img className='i1' src={require('./images/b1.png')} alt=""></img>
-                <div className='ic1'><img className='pict1' src={require('./images/b2.png')} alt=""></img></div> 
+                <div className='ic1'><img className='pict1' src={require('./images/b2.png')} alt=""></img></div>
+                <div className='ic2'><img className='pict2' src={require('./images/k1.png')} alt=""></img></div> 
+                <div className='ic3'><img className='pict3' src={require('./images/k2.png')} alt=""></img></div> 
+               
+                <div className='ic4'><img className='pict4' src={require('./images/k3.png')} alt=""></img></div>  
+                <div className='ic5'><img className='pict5' src={require('./images/k5.png')} alt=""></img></div>  
                 <div className='t-1'>
                     <div className='t-2'>Passion at <span className='sp'>Work</span></div>
                     <div className='t-3'><p>The design of buildings and the space within the site surrounding the buildings that have human occupancy or use as their principal purpose.The design of buildings and the space within the site.</p></div>
--- project/src/Components/Note.jsx
+++ project/src/Components/Note.jsx
@@ -7,17 +7,17 @@ export default function Whyus(){
             <div className='heading'>Why Us?</div>
             <div className='body'>
                 <div className='project'>
-                    <img className='icon-1' src={require('./images/icon1.png')} alt=""></img>
-                    <div className='text1'>Building Projects</div>
+                    <div className='p1'><img className='icon-1' src={require('./images/icon1.png')} alt=""></img></div>
+                    <div className='text1'>Building Project</div>
                     <div className='text2'>Baladi has more than 10+ active construction projects</div>
                 </div>
                 <div className='consultans'>
-                <img className='icon-2' src={require('./images/icon2.png')} alt=""></img>
+                <div className='c1'><img className='icon-2' src={require('./images/icon2.png')} alt=""></img></div>
                 <div className='text3'>Top Consultants</div>
                 <div className='text4'>Baladi has best builder consultant in India</div>
                 </div>
                 <div className='satisfaction'>
-                <img className='icon-3' src={require('./images/icon3.png')} alt=""></img>
+                <div className='s1'><img className='icon-3' src={require('./images/icon3.png')} alt=""></img></div>
                 <div className='text5'>Satisfaction</div>
                 <div className='text6'>We have more than 100+ satisfied customers</div>
                 </div>
