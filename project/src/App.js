5b997c3898c1e5682838ec44f717f20fa19699b1
ROOPAGOUDA12 <roopaggouda@gmail.com>
2023-05-04 11:48:41 +0530

    Baladi project about us and why us page responsive

--- project/src/App.js
+++ project/src/App.js
@@ -7,11 +7,14 @@ import Last from './Components/About2'
 
 function App() {
   return (
+
     <div className="App">
+      {/* <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta> */}
       <Whyus />
       <About />
       <Next />
       <Last />
+     
       
      
       
