08a8dc814bf1fba0aa2ff7ee0643bdf8f5de8132
ANUPAMASHETTY12 <ampushetty2004@gmail.com>
2023-05-19 14:27:51 +0530

    Added slider to award-recieved component

---
+++ Property/property-app/src/App.js
@@ -0,0 +1,23 @@
+// import logo from './logo.svg';
+import './App.css';
+
+import Award from "./Component/Award";
+// import Slider from './Component/Slider';
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <div>
+        <Award></Award>
+      </div>
+      {/* <div>
+        <Slider></Slider>
+
+      </div> */}
+    </div>
+  );
+}
+
+export default App;
