a63762b8d2bc216ccce971da3856a7ae877deecb
ROOPAGOUDA12 <roopaggouda@gmail.com>
2023-05-19 15:43:54 +0530

    why invest in mysuru page is 10% completed

--- Goldenproperty/project2/src/App.js
+++ Goldenproperty/project2/src/App.js
@@ -1,11 +1,13 @@
 // import logo from './logo.svg';
 import './App.css';
 import Whyus from './Components/Why'
+import Mysuru from './Components/Mysuru1'
 
 function App() {
   return (
     <div className="App">
      <Whyus/>
+     <Mysuru/>
     </div>
   );
 }
---
+++ Goldenproperty/project2/src/Components/Mysuru1.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Mysuru1.css';
+
+export default function Mysuru(){
+    return(
+        <div className='main-section'>
+            <div className='w-m'>Why Invest in Mysuru</div>
+            <div className='head-1'>The  Noteworthy Real Estate in India</div>
+            <div className='i'><img className='ic-1' src={require('./Images/mysur.png')} alt=""></img>
+            <div className='body2'>
+                <div className='body2-1'></div>
+                <div className='body2-2'></div>
+                <div className='body2-3'></div>
+            </div>
+            </div>
+        </div>
+    );
+}
