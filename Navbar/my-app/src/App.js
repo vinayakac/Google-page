1d63e70ce07819f6d42654e0834d2eadc9a1bef7
SAMIKSHANAIK01 <samikshanaik0112@gmail.com>
2023-05-19 09:50:26 +0530

    completed code for Baladi project

---
+++ Navbar/my-app/src/App.js
@@ -0,0 +1,17 @@
+// import logo from './logo.svg';
+import './App.css';
+import RNavbar from './Components/RNavbar/RNavbar';
+import Content from './Components/content/content';
+import SlickSlide from './Components/SlickCarousel/SlickCarousel';
+
+function App() {
+  return (
+    <div className="App1">
+      <RNavbar />
+      <Content />
+      <SlickSlide />
+    </div>
+  );
+}
+
+export default App;
---
+++ Navbar/my-app/src/Components/SlickCarousel/SlickCarousel.js
@@ -0,0 +1,64 @@
+import React from "react";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Slides_data from "./SlickCarousel.json";
+import './SlickCarousel.css'
+
+const SlickSlide = () => {
+    const settings = {
+        dots: false,
+        infinite: true,
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        responsive: [
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1,
+                    infinite: true,
+                    dots: false,
+                },
+            },
+            {
+                breakpoint: 410,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    infinite: true,
+                    dots: false,
+                },
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    infinite: true,
+                    dots: false,
+                },
+            },
+        ],
+    };
+
+    return (
+        <div className="slider">
+            
+            <h2>Services</h2> 
+            <Slider {...settings}>
+                {Slides_data.map((slide) => (
+                    <div key={slide.id}>
+                        <div className='service_container'>
+                            <img src={slide.image} alt=" " />
+                            <span className="service-details-heading"> {slide.title}</span>
+                            <span className="service-details-subheading">{slide.description}</span>
+                        </div>
+                    </div>
+                ))}
+            </Slider>
+        </div>
+    );
+};
+
+export default SlickSlide;
