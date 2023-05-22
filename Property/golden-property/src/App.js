ae3701957aee0812013b6b3118f864eeef7ae0be
ANUPAMASHETTY12 <ampushetty2004@gmail.com>
2023-05-22 14:17:05 +0530

    Contact-us component 80% completed

---
+++ Property/golden-property/src/App.js
@@ -0,0 +1,19 @@
+// import logo from './logo.svg';
+import './App.css';
+// import Award from './Component/Award.jsx'
+import Contact from './Component/Contact'
+
+function App() {
+  return (
+    <div className="App">
+      {/* <div>
+        <Award></Award>
+      </div> */}
+      <div>
+        <Contact></Contact>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ Property/golden-property/src/Component/Award.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import stick from '../Assests/stick.png';
+// import Picture1 from '../Assests/Picture1.png';
+// import Flower from '../Assests/flower.png';
+import stickr from '../Assests/stickr.png';
+import "./Award.css";
+import Slider from '../Component/Slider'
+
+
+const Award = () => {
+  return (
+    <div className="award">
+      <div className="award-title">
+        <div className="award-heading">Awards Received</div>
+        <div className="award-subheading">We have received many awards</div>
+      </div>
+      <div className="award-image">
+        <img src={stick} alt="Stick" className="stick-left" />
+        {/* <div className="award-image2">
+          <img src={Flower} alt="Flower" className="award-flower1" />
+          <div className="award-image3">
+            <img src={Picture1} alt="#" className="award-picture1" />
+          </div>
+        </div> */}
+        <div className='slide'>
+          <Slider></Slider>
+        </div>
+        <img src={stickr} alt="Stick Right" className="stick-right" />
+      </div>
+      
+    </div>
+  );
+}
+
+export default Award;
