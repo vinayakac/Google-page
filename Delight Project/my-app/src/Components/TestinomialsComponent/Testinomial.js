205f1801c8e42059927795bf53ff7b95f8324f3f
SAMIKSHANAIK01 <samikshanaik0112@gmail.com>
2023-05-30 21:19:48 +0530

    completed location component for delight capital solution project

--- Delight Project/my-app/src/Components/TestinomialsComponent/Testinomial.js
+++ Delight Project/my-app/src/Components/TestinomialsComponent/Testinomial.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import './Testinomial.css'
+// import Background from '../TestinomialsComponent/images/test-image-1.png'
 
 
 const Testinomial = () => {
   return (
     <div className='testinomial-container'>
-      <img src={Background} alt='#' className='testinomial-background'></img>
+      {/* <img src={Background} alt='#' className='testinomial-background'></img> */}
     </div>
   )
 }
